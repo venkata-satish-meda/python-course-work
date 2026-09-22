0314ea8fac0dfd2e8c7e4b84d09a8811d3ad88dd
Venkata Satish <Satishmeda46@gmail.com>
2026-09-22 12:22:29 +0530

    Sets and dictionaries

---
+++ Day-05/01_sets.py
@@ -0,0 +1,9 @@
+# Day 5 - Sets
+# A set stores unique values.
+
+numbers = {1, 2, 3, 3, 4}
+
+print(numbers)
+
+numbers.add(5)
+print(numbers)
---
+++ Day-05/02_set_operations.py
@@ -0,0 +1,9 @@
+# Day 5 - Set operations
+# Sets support union, intersection, and difference.
+
+a = {1, 2, 3}
+b = {3, 4, 5}
+
+print(a | b)
+print(a & b)
+print(a - b)
