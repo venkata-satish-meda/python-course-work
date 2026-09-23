0ff60621660a80d4e1b13d52ec77f238428711f8
Venkata Satish <Satishmeda46@gmail.com>
2026-09-23 17:31:52 +0530

    Data types and type conversion

---
+++ Day-03/datatype_practice.py
@@ -0,0 +1,8 @@
+"""
+Day-03 - datatype_practice.py
+
+Practice file.
+Add multiple programs for this concept, progressing from basic logic to realistic and combined problems.
+"""
+
+# Write your practice programs below.
---
+++ Day-03/type_conversion.py
@@ -0,0 +1,8 @@
+"""
+Day-03 - type_conversion.py
+
+Practice file.
+Add multiple programs for this concept, progressing from basic logic to realistic and combined problems.
+"""
+
+# Write your practice programs below.
