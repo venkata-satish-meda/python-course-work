f1db320940b2add0f83949843d45b496620140ff
Venkata Satish <Satishmeda46@gmail.com>
2026-09-23 17:31:56 +0530

    Functions parameters and arguments

---
+++ Day-14/parameters.py
@@ -0,0 +1,8 @@
+"""
+Day-14 - parameters.py
+
+Practice file.
+Add multiple programs for this concept, progressing from basic logic to realistic and combined problems.
+"""
+
+# Write your practice programs below.
