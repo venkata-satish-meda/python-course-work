d936f0da134292de1a9e5e522399a781c01068d8
Venkata Satish <Satishmeda46@gmail.com>
2026-09-23 17:32:01 +0530

    Constructors and encapsulation

---
+++ Day-25/encapsulation.py
@@ -0,0 +1,8 @@
+"""
+Day-25 - encapsulation.py
+
+Practice file.
+Add multiple programs for this concept, progressing from basic logic to realistic and combined problems.
+"""
+
+# Write your practice programs below.
---
+++ Day-25/self.py
@@ -0,0 +1,8 @@
+"""
+Day-25 - self.py
+
+Practice file.
+Add multiple programs for this concept, progressing from basic logic to realistic and combined problems.
+"""
+
+# Write your practice programs below.
