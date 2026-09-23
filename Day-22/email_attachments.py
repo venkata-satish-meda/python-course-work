0877462033fe2e1d8be00d1dad8e75339785b386
Venkata Satish <Satishmeda46@gmail.com>
2026-09-23 17:32:00 +0530

    Email automation

---
+++ Day-22/email_attachments.py
@@ -0,0 +1,8 @@
+"""
+Day-22 - email_attachments.py
+
+Practice file.
+Add multiple programs for this concept, progressing from basic logic to realistic and combined problems.
+"""
+
+# Write your practice programs below.
