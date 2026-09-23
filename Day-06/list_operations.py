300a45818664c7970000147e8bc0ee62ad49bc16
Venkata Satish <Satishmeda46@gmail.com>
2026-09-23 17:31:53 +0530

    Lists and tuples

---
+++ Day-06/list_operations.py
@@ -0,0 +1,8 @@
+"""
+Day-06 - list_operations.py
+
+Practice file.
+Add multiple programs for this concept, progressing from basic logic to realistic and combined problems.
+"""
+
+# Write your practice programs below.
---
+++ Day-06/list_tuple_practice.py
@@ -0,0 +1,8 @@
+"""
+Day-06 - list_tuple_practice.py
+
+Practice file.
+Add multiple programs for this concept, progressing from basic logic to realistic and combined problems.
+"""
+
+# Write your practice programs below.
